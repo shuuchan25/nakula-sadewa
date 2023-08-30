1102c6cdbd152e930267d213f9c4f3a5a324814d
shuuchan25 <ahnaftsaqif25@gmail.com>
2023-08-30 14:29:51 +0700

    ahnaf 8.30

--- resources/views/admin/partials/master.blade.php
+++ resources/views/admin/partials/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Nakula Sadewa</title>
+    <title>Dashboard Admin Nakula Sadewa</title>
 
     <!-- Scripts -->
     {{-- <link href="js/script.js" rel="stylesheet"> --}}
