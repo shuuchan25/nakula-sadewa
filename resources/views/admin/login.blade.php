d396920e8014910468719653d59c6a65f4a38acf
shuuchan25 <ahnaftsaqif25@gmail.com>
2023-08-30 16:24:34 +0700

    login

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,22 +1,24 @@
 @extends('admin.partials.master') {{-- Gantikan dengan layout yang Anda gunakan --}}
 
 @section('content')
-  <section class="login-page">
+    <section class="login-page">
         <form action="" method="">
             <div class="login-form">
-                <img src="../assets/img/User.png" alt="">
-            <div class="input-wrap">
-                <label for="email">Email</label>
-                <input type="email" name="email" required>
+                <div class="w-100">
+                    <h1>Login Admin</h1>
+                    <h2>Dashboard Nakula Sadewa</h2>
+                </div>
+                <div class="input-wrap">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" required>
+                </div>
+                <div class="input-wrap">
+                    <label for="password">Password</label>
+                    <input type="password" name="password">
+                </div>
+                <button type="submit" class="primary-button mt-3">Login</button>
             </div>
-            <div class="input-wrap">
-                <label for="password">Password</label>
-                <input type="password" name="password">
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </div>
         </form>
 
-  </section>
-
+    </section>
 @endsection
