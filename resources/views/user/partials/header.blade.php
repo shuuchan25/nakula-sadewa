6eea320a1aafee1699a771f0794a7b14ae4030d0
nisrinaawn <nwanadaa@gmail.com>
2023-09-01 10:34:50 +0700

    move icon to folder icons and change text color to white if hover on navbar

--- resources/views/user/partials/header.blade.php
+++ resources/views/user/partials/header.blade.php
@@ -31,27 +31,21 @@
 
 <nav class="navbar navbar-expand-lg">
   {{-- <div class="container"> --}}
-  <div class="container-fluid">
-    <div class="container">
-
-      <div class="rocket">
-        <img class="logo" src="../assets/img/Rocket.svg" alt="">
-      </div>
+  <div class="container">
+        <img class="logo" src="../assets/icons/Rocket.svg" alt="">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav mx-auto">
-
-        <a class="nav-link" href="#">Beranda</a>
-        <a class="nav-link" href="#">Eksplorasi</a>
-        <a class="nav-link" href="#">FAQ</a>
-        <a class="nav-link" href="#">Kontak</a>
-        <a class="nav-link" href="#">Petunjuk</a>
+        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.hide">Beranda</a>
+        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.hide">Eksplorasi</a>
+        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.hide">FAQ</a>
+        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.hide">Kontak</a>
+        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target=".navbar-collapse.hide">Petunjuk</a>
       </div>
       <div class="cart">
-        <img src="../assets/img/Cart.svg" alt="">
+        <img src="../assets/icons/Cart.svg" alt="">
       </div>
     </div>
   </div>
