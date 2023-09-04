5ac7257cf6909ba7267c96f39fa4dba728d4c13b
shanglandrea <shanglandrea@gmail.com>
2023-09-04 14:57:27 +0700

    fe-dea

---
+++ resources/views/user/kuliner.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Kuliner</title>
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Header-->
+        <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Kuliner</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">Temukan Makanan blablablabla</p>
+                </div>
+            </div>
+        </header>
+
+        <div class='container'>
+            <div class="container" style="margin: 0px 80px 0px 80px;">
+            <div class="card">
+                <!-- Product image-->
+                    <img src="img/1.png"/>
+                <!-- Product details-->
+                    <div class='card-body'>
+                    <div class='card-text'>
+                    <h3>Lorem ipsum</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                    It has survived not only five centuries, but also the leap into electronic typesetting, 
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
+                    like Aldus PageMaker including versions of Lorem Ipsum.In porttitor porta mi pharetra tempor.</p>   
+                    </div>
+                <!-- Product actions-->
+                    <button>More...</button>
+            </div>
+        </div>     
+
+            <div class="card">
+                <!-- Product image-->
+                    <img src="img/2.png"/>
+                <!-- Product details-->
+                    <div class='card-body'>
+                    <div class='card-text'>
+                    <h3>Lorem ipsum</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                    It has survived not only five centuries, but also the leap into electronic typesetting, 
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
+                    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
+                    like Aldus PageMaker including versions of Lorem Ipsum.In porttitor porta mi pharetra tempor.</p>   
+                    </div>
+                <!-- Product actions-->
+                    <button>More...</button>
+            </div>
+        </div>
+    </div>   
+        </div>
+                <div style="clear: both;"></div>  
+        <!-- Footer-->
+        
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
