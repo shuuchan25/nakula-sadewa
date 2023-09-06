f7a35cc8858eea2a4509b073829c82b1185d2797
shanglandrea <shanglandrea@gmail.com>
2023-09-06 14:12:10 +0700

    kuliner

--- resources/views/user/kuliner.blade.php
+++ resources/views/user/kuliner.blade.php
@@ -44,56 +44,74 @@
             </div>
         </div>
 
-
-
-        <!-- Section-->
-        <div class='container'>
-            <div class="container" style="margin: 0px 80px 0px 80px;">
-            <div class="card">
-                <!-- Product image-->
-                    <img src="img/1.png"/>
-                <!-- Product details-->
-                    <div class='card-body'>
-                    <div class='card-text'>
-                    <h3>Lorem ipsum</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                    It has survived not only five centuries, but also the leap into electronic typesetting, 
-                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
-                    like Aldus PageMaker including versions of Lorem Ipsum.In porttitor porta mi pharetra tempor.</p>   
-                    </div>
-                <!-- Product actions-->
-                    <button>More...</button>
+        {{-- SEARCH --}}
+            <div class="container">   
+                <p><h3>Temukan Makanan Favoritmu Disini!</h3></p> 
             </div>
-        </div>     
-
-            <div class="card">
-                <!-- Product image-->
-                    <img src="img/2.png"/>
-                <!-- Product details-->
-                    <div class='card-body'>
-                    <div class='card-text'>
-                    <h3>Lorem ipsum</h3>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-                    It has survived not only five centuries, but also the leap into electronic typesetting, 
-                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-                    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software 
-                    like Aldus PageMaker including versions of Lorem Ipsum.In porttitor porta mi pharetra tempor.</p>   
+        <section class="search" style="margin: 0px 80px 0px 80px;">
+            <div class="mx-auto col-12 justyfy-content-center align-item-center">
+                    {{-- <span class="input-group-text"> --}}
+                    <div class="row">
+                        <!-- <div class="col-lg cari">
+                            <h4 class="text my-auto">Cari Destinasi Wisata</h4>
+                        </div> -->
+                            <div class="col-lg cari">
+                                <button class="btn nada btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Kategori Makanan</button>
+                                    <ul class="dropdown-menu" style="width: 300px; text-align:center;">
+                                    <li><a class="dropdown-item" href="#">Makanan</a></li>
+                                    <li><a class="dropdown-item" href="#">Minuman</a></li>
+                                    <li><a class="dropdown-item" href="#">Snack</a></li>
+                                    <li><a class="dropdown-item" href="#">Paket</a></li>
+                                    </ul>
+                            </div>
+                            <div class="col-lg cari">
+                                <div class="input-container">
+                                    <input type="text" class="form-control" placeholder="Cari Makanan" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    <img src="../assets/icons/Search-bl.svg" id="input_img">
+                                </div>
+                            </div>
                     </div>
-                <!-- Product actions-->
-                    <button>More...</button>
+                    {{-- </span> --}}
             </div>
-        </div>
-    </div>   
-        </div>
-                <div style="clear: both;"></div>
+        </section>
 
-        <!-- Footer-->
-        {{-- END KALENDER EVENT --}}
+        <!-- CARD RESTAURANT-->
+        <section class="cardkuliner" style="margin: 0px 80px 0px 80px;">
+            <div class="container cardlist d-flex mt-4 p-4">
+                <div class="card mb-3">
+                    <div class="row card2 g-0">
+                        <div class="col-md-5 my-auto">
+                            <img src= "../assets/pict/hero-wisata.jpg" alt="poto wisata">
+                        </div>
+                        <div class="col-md-6 mx-auto">
+                            <div class="card-body">
+                                <h5 class="card-title">Resto</h5>
+                                <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus laborum recusandae ad, dicta ratione deserunt in tempora aspernatur omnis quia necessitatibus iusto est earum alias. Tenetur eligendi sit minima dolorem!</p>
+                                <!-- <button type="button" class="btn btn-primary">Lihat Detail</button> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card mb-3">
+                    <div class="row card2 g-0">
+                        <div class="col-md-5 my-auto">
+                            <img src= "../assets/pict/hero-deswisata.png" alt="poto wisata">
+                        </div>
+                        <div class="col-md-6 mx-auto">
+                            <div class="card-body">
+                                <h5 class="card-title">Resto</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, rem eveniet? Aut soluta doloremque quas. Illum cumque repellendus vitae accusantium possimus nobis doloremque earum tempora rem numquam ut, velit expedita.</p>
+                                <!-- <button type="button" class="btn btn-primary">Lihat Detail</button> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+                <div style="clear: both;"></div>  
+
+        <!-- FOOTER-->
 
 @include('user.partials.footer')
 
