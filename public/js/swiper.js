3ee976e18b2ab4cd5cd9eeb36c8c1a08cf23a475
nisrinaawn <nwanadaa@gmail.com>
2023-09-10 16:02:20 +0700

    homepage done (minus responsive)

--- public/js/swiper.js
+++ public/js/swiper.js
@@ -1,16 +1,11 @@
-var swiper = new Swiper(".slide-container", {
-    slidesPerView: 4,
+var swiper = new Swiper(".slide-container-kalender", {
+    slidesPerView: 3,
     spaceBetween: 20,
-    sliderPerGroup: 4,
+    sliderPerGroup: 3,
     loop: true,
     centerSlide: "true",
     fade: "true",
     grabCursor: "true",
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -28,6 +23,62 @@ var swiper = new Swiper(".slide-container", {
         },
             1000: {
             slidesPerView: 4,
+        },
+    },
+});
+
+var swiper = new Swiper(".slide-container-berita", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    sliderPerGroup: 3,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    autoplay: true,
+
+    breakpoints: {
+            0: {
+                slidesPerView: 1,
+        },
+            520: {
+            slidesPerView: 2,
+        },
+            768: {
+            slidesPerView: 3,
+        },
+            1000: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".slide-container-komentar", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    sliderPerGroup: 3,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+            0: {
+                slidesPerView: 1,
+        },
+            520: {
+            slidesPerView: 2,
+        },
+            768: {
+            slidesPerView: 3,
+        },
+            1000: {
+            slidesPerView: 3,
         },
     },
 });
