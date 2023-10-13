65ccc99bb33af25a62955b31073440968eebb2aa
shanglandrea <shanglandrea@gmail.com>
2023-10-13 12:28:52 +0700

    button search kuliner

--- resources/views/detailrumahmakan.blade.php
+++ resources/views/detailrumahmakan.blade.php
@@ -4,33 +4,33 @@
 {{-- Get partials --}}
 @include('partials.header')
 
-{{-- BREADCRUMB --}}
-<div class="container">
-    <div style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
-        <ul class="breadcrumb">
-        <li class="breadcrumb-item"><a style="text-decoration:none" href="../rumahmakan">Kuliner</a></li>
-        <li class="breadcrumb-item"><a style="text-decoration:none" href="../rumahmakan">Rumah Makan</a></li>
-        <li class="breadcrumb-item" aria-current="page">Detail</li>
-        </ul>
+<!-- BREADCRUMB -->
+    <div class="container">
+        <div style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <ul class="breadcrumb">
+            <li class="breadcrumb-item"><a style="text-decoration:none" href="../rumahmakan">Kuliner</a></li>
+            <li class="breadcrumb-item"><a style="text-decoration:none" href="../rumahmakan">Rumah Makan</a></li>
+            <li class="breadcrumb-item" aria-current="page">Detail</li>
+            </ul>
 
 <!-- HERO-->
-        <div class="resto-img col-md-12 relative mb-3">
-            <img src="../assets/pict/hero-wisata.jpg" alt="Rumah Makan"/>
-            <div class="content">
-                <div class="button-back">
-                    <button onclick="window.location='rumahmakan'" class="btn-back">
-                        <svg width="20" height="25" viewBox="0 0 36 41" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M34.1287 20.3381H2M2 20.3381L17.4218 2M2 20.3381L17.4218 38.6763" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </button>
-                </div>
-                <div class=>
-                    <h1 class="heading">Rumah Makan</h1>
+            <div class="resto-img col-md-12 relative mb-3">
+                <img src="../assets/pict/hero-wisata.jpg" alt="Rumah Makan"/>
+                <div class="content">
+                    <div class="button-back">
+                        <button onclick="window.location='rumahmakan'" class="btn-back">
+                            <svg width="20" height="25" viewBox="0 0 36 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M34.1287 20.3381H2M2 20.3381L17.4218 2M2 20.3381L17.4218 38.6763" stroke="black" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <div class=>
+                        <h1 class="heading">Rumah Makan</h1>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 <!-- DESCRIPTION RESTAURANT-->
     <section class="deskripsi mt-5 mb-5 pt-3 pb-3">
@@ -197,43 +197,39 @@
                 </div>
         </div>
 
+    <!-- FOOTER-->
 
+    @include('partials.footer')
+    @endsection
 
-        <div style="clear: both;"></div>
+    @section('script-body')
+    <script>
 
- <!-- FOOTER-->
+        // Ambil semua elemen kartu produk
+        const cardkuliner = document.querySelectorAll(".card-3");
 
-@include('partials.footer')
-@endsection
+        // Loop melalui setiap kartu produk dan tambahkan fungsionalitas
+        cardkuliner.forEach((document) => {
+        const plus = document.querySelector(".plus");
+        const minus = document.querySelector(".minus");
+        const num = document.querySelector(".num");
 
-@section('script-body')
-<script>
+        let a = 1; // Jumlah awal produk
 
-    // Ambil semua elemen kartu produk
-    const cardkuliner = document.querySelectorAll(".card-3");
-
-    // Loop melalui setiap kartu produk dan tambahkan fungsionalitas
-    cardkuliner.forEach((document) => {
-    const plus = document.querySelector(".plus");
-    const minus = document.querySelector(".minus");
-    const num = document.querySelector(".num");
-
-    let a = 1; // Jumlah awal produk
-
-    plus.addEventListener("click", () => {
-        a++;
-        num.textContent = a;
-    });
-
-    minus.addEventListener("click", () => {
-        if (a > 1) {
-            a--;
+        plus.addEventListener("click", () => {
+            a++;
             num.textContent = a;
-        }
-    });
-    });
+        });
 
-</script>
+        minus.addEventListener("click", () => {
+            if (a > 1) {
+                a--;
+                num.textContent = a;
+            }
+        });
+        });
 
-@endsection
+    </script>
+
+    @endsection
 
