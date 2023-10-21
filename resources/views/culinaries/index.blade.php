7930580925636b8ccb71aaeddec539a36bb4b564
myname <amel71268@gmail.com>
2023-10-21 23:28:36 +0700

    nata html pills & search atraksi tp mboh sek bingung maaf jika salah ya ges yak

--- resources/views/culinaries/index.blade.php
+++ resources/views/culinaries/index.blade.php
@@ -25,7 +25,7 @@
                 <form action="/culinaries" method="GET">
                 @csrf
                     <div class="container search-all">
-                        <h4 class="title-heading">Temukan Rumah Makan Favoritmu di Sini!</h4>
+                        {{-- <h4 class="title-heading">Temukan Rumah Makan Favoritmu di Sini!</h4> --}}
                         <div class="searchbar d-flex mt-3 w-100 justify-content-center">
                             <div class="searchinput" style="width: 80%">
                                 <input name="search" class="form-control me-2" type="search" placeholder="Cari Rumah Makan" aria-label="Search" value="{{ request('search') }}">
