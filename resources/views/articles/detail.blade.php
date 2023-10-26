e2bbc549f0d2f842c2e2b0513ea5989ed6505be6
nisrinaawn <nwanadaa@gmail.com>
2023-10-26 15:09:29 +0700

    benerin detail hero, shadow

--- resources/views/articles/detail.blade.php
+++ resources/views/articles/detail.blade.php
@@ -18,7 +18,7 @@
 
             {{-- hero --}}
             <div class="detail row">
-                <div class="banner col-md-12">
+                <div class="banner cerita-artikel col-md-12">
                     <img style="filter: none" src="{{ Storage::url($article->image) }}" alt="Berita Terkini"/>
                     <div class="content">
                         <div class="button-balik">
