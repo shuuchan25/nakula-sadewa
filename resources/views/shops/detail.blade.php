3af72bca3dd893302309a6354863cd80baf2ef25
nisrinaawn <nwanadaa@gmail.com>
2023-12-04 13:42:03 +0700

    maps + desc atraksi

--- resources/views/shops/detail.blade.php
+++ resources/views/shops/detail.blade.php
@@ -80,10 +80,6 @@
 
 {{-- MAPS --}}
 <div class="container mt-3">
-    <section class="maps mt-5 pt-4 mb-5">
-        <h5 class="card-title pb-3">Lokasi/Maps</h5>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252760.8614014133!2d111.47004232617756!3d-8.163560432211733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e791ad33bad6389%3A0x19f173f90f85d9be!2sTrenggalek%2C%20Kabupaten%20Trenggalek%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1697517099184!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="rounded-4"></iframe>
-    </section>
 
 <!-- CARD LIST -->
         <h5 class="judul-produk mt-5 mb-3">Produk</h5>
@@ -119,6 +115,10 @@
                 <p class="d-flex justify-content-center align-item-center mt-5">Belum ada data Menu yang tersedia.</p>
             @endif
 
+        <section class="maps mt-5 pt-4 mb-5">
+            <h5 class="card-title pb-3">Lokasi/Maps</h5>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252760.8614014133!2d111.47004232617756!3d-8.163560432211733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e791ad33bad6389%3A0x19f173f90f85d9be!2sTrenggalek%2C%20Kabupaten%20Trenggalek%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1697517099184!5m2!1sid!2sid" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="rounded-4"></iframe>
+        </section>
 </div>
 {{-- end container --}}
 
